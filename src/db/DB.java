c8cc74620f1f767a22af24f8840155e8d67af7fa
Heitor Martini <martiniheitor@gmail.com>
2024-09-22 18:48:53 -0300

    FindById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -12,14 +12,14 @@ import java.util.Properties;
 public class DB {
 
 	private static Connection conn = null;
-
+	
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			} 
+			}
 			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
@@ -31,19 +31,18 @@ public class DB {
 		if (conn != null) {
 			try {
 				conn.close();
-			} 
-			catch (SQLException e) {
-				throw new DbIntegrityException(e.getMessage());
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
 			}
 		}
 	}
-
+	
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} 
+		}
 		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
@@ -58,7 +57,7 @@ public class DB {
 			}
 		}
 	}
-	
+
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
 			try {
@@ -68,5 +67,4 @@ public class DB {
 			}
 		}
 	}
-
 }
